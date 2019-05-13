e49ceba3eef611aa1b9c323b62052845fbbd0e71
zx <petsgre@gmail.com>
2019-05-13 17:55:48 +0800

    函数内声明组件

--- src/components/HOC/Box.tsx
+++ src/components/HOC/Box.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 
+function Test(){
+  return (
+    <div>123</div>
+  )
+}
 export default function (InnerComponent: any) {
   return class Box extends React.Component<any, object> {
     public name = 'Box'
@@ -12,6 +17,7 @@ export default function (InnerComponent: any) {
         <div className="box">
           这是高阶组件容器
           <InnerComponent type={type} {...other} />
+          <Test/>
         </div>
       )
     }
